acf534dfc2a846aaec3b139d23c72319ea9c616d
Igor Tuag <igortuag@gmail.com>
2020-07-28 18:47:57 -0300

    Slots

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,8 +6,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendess.length }} attending</span>
+      <BaseIcon name="users">{{ event.attendess.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
