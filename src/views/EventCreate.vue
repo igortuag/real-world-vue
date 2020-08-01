683c3d04449b1f8d4c91a89c7e8424b4db04e38c
Igor Tuag~ <igortuag@gmail.com>
2020-08-01 18:20:57 -0300

    Completing Progress Bar

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -10,18 +10,30 @@
       <h3>Name & describe your event</h3>
       <div class="field">
         <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title" />
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
       </div>
 
       <div class="field">
         <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description" />
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
       </div>
 
       <h3>Where is your event?</h3>
       <div class="field">
         <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location" />
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
       </div>
 
       <h3>When is your event?</h3>
@@ -45,6 +57,7 @@
 
 <script>
 import Datepicker from 'vuejs-datepicker'
+import NProgress from 'nprogress'
 
 export default {
   components: {
@@ -63,6 +76,7 @@ export default {
   },
   methods: {
     createEvent() {
+      NProgress.start()
       this.$store
         .dispatch('event/createEvent', this.event)
         .then(() => {
@@ -72,7 +86,9 @@ export default {
           })
           this.event = this.createFreshEventObject()
         })
-        .catch(() => {})
+        .catch(() => {
+          NProgress.done()
+        })
     },
     createFreshEventObject() {
       const user = this.$store.state.user.user
