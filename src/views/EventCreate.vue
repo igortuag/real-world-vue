90f6472fb550c52570620560c6e89369dc71896a
Igor Tuag~ <igortuag@gmail.com>
2020-08-01 17:04:17 -0300

    Global Guards

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -10,30 +10,18 @@
       <h3>Name & describe your event</h3>
       <div class="field">
         <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
+        <input v-model="event.title" type="text" placeholder="Add an event title" />
       </div>
 
       <div class="field">
         <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
+        <input v-model="event.description" type="text" placeholder="Add a description" />
       </div>
 
       <h3>Where is your event?</h3>
       <div class="field">
         <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
+        <input v-model="event.location" type="text" placeholder="Add a location" />
       </div>
 
       <h3>When is your event?</h3>
@@ -56,7 +44,7 @@
 </template>
 
 <script>
-import { Datepicker } from 'vuejs-datepicker'
+import Datepicker from 'vuejs-datepicker'
 
 export default {
   components: {
